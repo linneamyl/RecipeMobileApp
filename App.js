39e9fc117c1eb5f25126300b329ca8f58c35c220
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-11-30 13:08:35 +0200

    Add Routes to another pages

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen'
+import ListScreen from './components/ListScreen'
+import RecipeScreen from './components/RecipeScreen'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Homepage" component={HomeScreen} />
+        <Stack.Screen name="Recipeliste" component={ListScreen} />
+        <Stack.Screen name="Recipe" component={RecipeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
