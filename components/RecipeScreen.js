cb7078b0a2261d149b7ab1f8900b0d17301afcce
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-12-03 15:06:55 +0200

    Update stuff

---
+++ components/RecipeScreen.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { View,
+         Text,  
+         StyleSheet, 
+         FlatList } 
+from 'react-native';
+
+export default function RecipeScreen() {
+
+    return(
+        <View></View>
+    )
+}
