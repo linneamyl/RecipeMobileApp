c1c0757d87ed0b42d1d08f135d96495ae264de13
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-12-03 15:05:37 +0200

    Change names of navigation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen'
@@ -13,9 +12,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Homepage" component={HomeScreen} />
-        <Stack.Screen name="Recipeliste" component={ListScreen} />
-        <Stack.Screen name="Recipe" component={RecipeScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ListScreen" component={ListScreen} />
+        <Stack.Screen name="RecipeScreen" component={RecipeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
