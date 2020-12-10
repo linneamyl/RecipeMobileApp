327356f03196a69623e03ed60ea91ddc34a776ed
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-12-10 11:32:13 +0200

    Picker etusivulle, josta voi valita alueen mukaan reseptejä

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -2,45 +2,110 @@ import React, { useEffect, useState } from 'react';
 import { View,
          Text,  
          StyleSheet, 
-         FlatList } 
+         FlatList,
+         Alert
+         } 
 from 'react-native';
+import { Picker } from '@react-native-picker/picker'
 import { SearchBar, Button, Tile } from 'react-native-elements';
 
 export default function HomeScreen({ navigation }) {
 
     const [searchQuery, setSearchQuery] = useState('')
     const [searchResults, setSearchResults] = useState([])
+    const [areas, setAreas] = useState([])
+    const [chosenArea, setChosenArea] = useState('')
 
-    function searchRecipes ()  {
-        console.log(searchQuery)
+
+    // Pickerin alueet haetaan aina kun sivu avataan
+    useEffect(() => {
+      fetchAreas()
+    },[]);
+
+    const fetchAreas = () => {
+      fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+      .then(response => response.json())
+      .then(responseData => {
+        setAreas(responseData.meals)
+      })
+      .catch(error => {
+        Alert.alert(error.message)
+      })
+    }
+
+
+
+    // Hakee Arean mukaan 
+    const searchByArea = () => {
+      fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=' + chosenArea)
+      .then((response) => response.json())
+      .then((responseData) => {
+        setSearchResults(responseData.meals)
+      })
+      .catch((error) => {
+        Alert.alert('Error', error)
+      });
+      navigation.navigate("ListScreen", {data: searchResults})
+    }
+
+
+
+
+
+    // Hakutoiminnon funktio
+    const searchRecipes = () =>  {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchQuery)
         .then((response) => response.json())
         .then((responseData) => {
           setSearchResults(responseData.meals)
-          console.log(searchResults)
+          //console.log(searchResults)
         })
         .catch((error) => {
-          Alert.alert('Error', error)
+          Alert.alert('Error', error.message)
         });
         navigation.navigate("ListScreen", {data: searchResults})
       }
 
+    
+
     return (
         <View>
             <Tile
-                imageSrc={require('../assets/foodpic.png') }
-                title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolore exercitationem"
-                featured caption="Some Caption Text"
+                imageSrc={require('../assets/foodpic.png')}
+                title="Recipe Finder"
+                featured caption="Find the best food recipes for everyday occasions"
             />
             <SearchBar
                 placeholder="Search recipes..."
+                onChangeText={value => setSearchQuery(value)}
                 value={searchQuery}
-                onChangeText={setSearchQuery}
             />
             <Button 
                 raised icon = {{name: 'search'}}
-                onPress={searchRecipes} 
+                onPress={searchRecipes}
                 title="SEARCH"/>
+            <Picker 
+                style={{height: 30, width: 150, marginTop: 40}} 
+                onValueChange={selectedValue => setChosenArea(selectedValue)}
+                selectedValue={chosenArea}>
+                {
+                  areas.map((item) =>
+                    <Picker.Item key={item} label={item.strArea} value={item.strArea} />)
+                }
+            </Picker>
+            <Button 
+                raised icon = {{name: 'search'}}
+                onPress={searchByArea}
+                title="SEARCH BY AREA"/>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  }
+})
