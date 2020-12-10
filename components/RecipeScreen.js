49526f30d6c3ab783dd97350b395493c3337d5a4
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-12-10 11:33:05 +0200

    RecipeScreenille fetch reseptin id:n mukaan

--- components/RecipeScreen.js
+++ components/RecipeScreen.js
@@ -1,13 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import { View,
          Text,  
-         StyleSheet, 
+         StyleSheet,
+         Image,
+         Alert,
+         ActivityIndicator, 
          FlatList } 
 from 'react-native';
 
-export default function RecipeScreen() {
+export default function RecipeScreen({ route, navigation }) {
 
+    const { propsItem } = route.params;
+    let itemId = propsItem.item.idMeal
+    
+    const [recipeById, setRecipeById] = useState({})
+    
+    console.log(recipeById, 'TÄMÄ')
+    // let id = recipeById[0].idMeal
+    // console.log(id, 'ID')
+    // const image = { uri: recipeById[0].strMealThumb };
+    // console.log(image, 'IMAGE')
+    let image = { uri: propsItem.item.strMealThumb };
+
+    function fetchRecipeById(){
+        fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + itemId)
+        .then((response) => response.json())
+        .then((responseData) => {
+          setRecipeById(responseData.meals[0])
+          console.log(recipeById)
+        })
+        .catch((error) => {
+          Alert.alert('Error', error.message)
+        });
+    }
+
+    useEffect(() => {
+        fetchRecipeById();
+      }, []); 
+
+  
+  
     return(
-        <View></View>
+        <View style={styles.RecipeScreenContainer}>
+            <Image
+              style={styles.Image}
+              source={image}
+              progressiveRenderingEnabled={true}
+            />
+            <Text>moro</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    RecipeScreenContainer: {
+      flex: 1,
+    },
+    ActivityIndicator: {
+      flex: 1,
+    },
+    Image: {
+      flex: 1,
+    
+    },
+    Infobox: {
+      padding: 5,
+    },
+  });
+  
