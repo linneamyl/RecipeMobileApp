56d4df94b973fdac0b9e62ccb9fea128dd94faf6
Linnea Myllynen <55977969+linneamyl@users.noreply.github.com>
2020-12-15 11:48:23 +0200

    koodin siivous

--- components/ListScreen.js
+++ components/ListScreen.js
@@ -1,25 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { View,
-         Text,  
+import React, { useState } from 'react';
+import { View, 
          StyleSheet, 
          FlatList } 
 from 'react-native';
 import Recipe from './Recipe';
 
-export default function ListScreen({navigation, route}) {
+export default function ListScreen({ route}) {
     const [recipeList, setRecipeList] = useState([])
-  
     const {data} = route.params
 
     React.useEffect(() => {
           setRecipeList(data)
-        //  console.log(data, 'DATA')
       }, []);
 
-   
     const renderItem = (item) => {
         return <Recipe item={item}/>;
     };
+
+    const listSeparator = () => {
+        return (
+          <View
+            style={{
+              height: 1,
+              width: "80%",
+              backgroundColor: "#CED0CE",
+              marginLeft: "10%"
+            }}
+          />
+        );
+      };
 
     return(
         <View style={styles.RecipeListContainer}>
@@ -28,6 +37,7 @@ export default function ListScreen({navigation, route}) {
                 data={recipeList}
                 keyExtractor={(item) => item.idMeal}
                 renderItem={({item}) => renderItem(item)}
+                ItemSeparatorComponent={listSeparator}
             ></FlatList>
         </View>
     )
